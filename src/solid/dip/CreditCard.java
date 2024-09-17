df40ce674f43884f58a3f6b41e5a9dd76451b404
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-09-17 12:04:59 +0100

    Tight Coupingwith cards not following Dependency Inversion Principle

---
+++ src/solid/dip/CreditCard.java
@@ -0,0 +1,7 @@
+package solid.dip;
+
+public class CreditCard {
+    public void doTransaction(long amount) {
+        System.out.println("payment using credit card");
+    }
+}
---
+++ src/solid/dip/ShoppingMall.java
@@ -0,0 +1,23 @@
+package solid.dip;
+
+public class ShoppingMall {
+
+    private DebitCard debitCard;
+    private CreditCard creditCard;
+
+    public ShoppingMall(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public void doPurchaseSomething(long amount) {
+        creditCard.doTransaction(amount);
+    }
+
+    public static void main(String[] args) {
+        DebitCard debitCard = new DebitCard();
+        CreditCard creditCard = new CreditCard();
+        ShoppingMall shoppingMall = new ShoppingMall(creditCard);
+        shoppingMall.doPurchaseSomething(5000);
+    }
+
+}
