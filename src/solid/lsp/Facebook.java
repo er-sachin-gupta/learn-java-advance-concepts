d4e5fd861bb5c257ae768033bb5992c38681faaf
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-09-17 11:00:50 +0100

    Liskov Sunsitutuon Principle related

---
+++ src/solid/lsp/Facebook.java
@@ -0,0 +1,24 @@
+package solid.lsp;
+
+// This facebook can replace SocialMedia which is its base class
+public class Facebook extends SocialMedia {
+    @Override
+    public void chatWithFriend() {
+
+    }
+
+    @Override
+    public void publishPost() {
+
+    }
+
+    @Override
+    public void sendPhotosAndVideos() {
+
+    }
+
+    @Override
+    public void groupVideoCall() {
+
+    }
+}
