c329e69618a51f83178ddf424048066700026151
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-09-19 01:45:14 +0100

    Local Variable Not initialized Issue fixed by making it global

--- src/com/semanticsquare/Student.java
+++ src/com/semanticsquare/Student.java
@@ -1,9 +1,10 @@
 package com.semanticsquare;
 
 public class Student {
+    int id; // Make it global variable
 
     void compute() {
-        int id;
+//        int id = 1000; Another Fix
         int nextId = id + 1; // Giving error because id variable has not been initialized manually
         // and also as it is a local variable it has not been initialized automatically, if it will be a global or static variable it will surely get default value
         System.out.println(id);
