9ef2c88621687b7d7d76eaf53feeaa4afc90eaa4
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-09-24 11:00:01 +0100

    FLoating Point no.s and Char Datatype

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -1,5 +1,7 @@
 package com.semanticsquare.basics;
 
+import java.math.BigDecimal;
+
 public class BasicsDemo {
 
     static void primitives() {
@@ -10,9 +12,33 @@ public class BasicsDemo {
         // Java 7
         int intBinary = 0b01000001;
         System.out.println("intBinary :  " + intBinary);
+
+        int intChar = 'A';
+        System.out.println("intChar " + intChar);
+
+        char charInt = 0;
+        System.out.println("charInt : " + charInt);
+
+        char charHex = 0x0041;
+        System.out.println("charHex : " + charHex);
+
+        char charBinary = 0b0100_0001;
+        System.out.println("charBinary : " + charBinary);
     }
 
     public static void main(String[] args) {
         primitives();
+        System.out.println(1 - 0.9);
+        System.out.println(0.1 + 0.2);
+
+        double price = 1000;
+        double discountPercent = 0.9;
+        double discountAmount = price * discountPercent;
+        System.out.println(price - discountAmount);
+        System.out.println(price * (1 - discountPercent));
+
+        BigDecimal first = new BigDecimal("0.1");
+        BigDecimal second = new BigDecimal("0.2");
+        System.out.println(first.add(second));
     }
 }
--- src/com/semanticsquare/basics/Student.java
+++ src/com/semanticsquare/basics/Student.java
@@ -7,17 +7,15 @@ public class Student {
      */
     byte age = 18;
 //    byte rank = 165; // possible lossy conversion from int to byte Error becuase byte can hold upto -128 to 127
-    short rank = 165;
 //    int phone = 2234567890; //integer number too large: 2234567890
     long phone = 223_456_7890L;
 
     // Integer Literals : int literal, long literal
 
-//    int minValue = Integer.MIN_VALUE;
-//    int maxValue = Integer.MAX_VALUE;
+    double gpa = 3.8;
 
-    byte minValue = Byte.MIN_VALUE;
-    byte maxValue = Byte.MAX_VALUE;
+//    char degree = '\u0000';
+    char degree = 'B';
 
     void compute() {
         int nextId = id + 1;
@@ -25,8 +23,10 @@ public class Student {
         System.out.println("nextId: " + nextId);
         System.out.println("age: " + age);
         System.out.println("Phone : " + phone);
-        System.out.println("MinValue : " + minValue);
-        System.out.println("MaxValue : " + maxValue);
+        System.out.println("gpa : " + gpa);
+        System.out.println("degree : " + degree);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
     }
 
     public static void main(String[] args) {
