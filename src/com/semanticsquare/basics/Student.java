301c589af9a81cc16de990cbeab0d2662a70d960
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-09-28 09:15:08 +0100

    Null Pointer Exception generated

--- src/com/semanticsquare/basics/Student.java
+++ src/com/semanticsquare/basics/Student.java
@@ -55,6 +55,8 @@ public class Student {
         System.out.println("computeCount : " + computeCount);
     }
 
+    static Student student3;
+
     public static void main(String[] args) {
         Student student1 = new Student();
         student1.id = 1000;
@@ -78,7 +80,7 @@ public class Student {
         student2.international = true;
         student2.compute();
 
-        Student student3 = new Student();
+//        Student student3 = new Student();
         student3.id = 1002;
         student3.name = "Anita";
         student3.gender = "female";
