20261067e0c8599a1417ee46e8c2d4e128b88625
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-10-03 12:52:11 +0100

    Instance and Static Methods

--- src/com/semanticsquare/basics/CurrencyConverter.java
+++ src/com/semanticsquare/basics/CurrencyConverter.java
@@ -2,46 +2,51 @@ package com.semanticsquare.basics;
 
 public class CurrencyConverter {
 
-   /* int rupee = 63;
+    static double[] exchangeRates;
 
-    int dirham = 3; // UAE
-
-    int real = 3;  // brazilian
-
-    int chilean_peso = 595;
-
-    int mexican_peso = 18;
-
-    int _yen = 107;
-
-    int $australian = 2;  // australian dollar
-
-    int dollar;
-
-    int Rupee = 63;*/
-
-    double[] exchangeRates = {63.0, 3.0, 3.0, 595.5, 18.0, 107.0, 2.0};
-
-    public static void main(String[] args) {
-        CurrencyConverter cc = new CurrencyConverter();
-        cc.printCurrencies();
+    void setExchangeRates(double[] rates) {
+        exchangeRates = rates;
     }
 
-    void printCurrencies() {
-        System.out.println("rupee: " + exchangeRates[0]);
+    void updateExchangeRates(int arrayIndex, double newVal) {
+        exchangeRates[arrayIndex] = newVal;
+    }
+
+    double getExchangeRate(int arrayIndex) {
+        return exchangeRates[arrayIndex];
+    }
+
+    double computeTransferAmount(int arrayIndex, double amount) {
+        return amount * getExchangeRate(arrayIndex);
+    }
+
+    static void printCurrencies() {
+        System.out.println("\n\nrupee: " + exchangeRates[0]);
         System.out.println("dirham: " + exchangeRates[1]);
         System.out.println("real: " + exchangeRates[2]);
         System.out.println("chilean_peso: " + exchangeRates[3]);
         System.out.println("mexican_peso: " + exchangeRates[4]);
         System.out.println("_yen: " + exchangeRates[5]);
         System.out.println("$australian: " + exchangeRates[exchangeRates.length-1]);
-//        System.out.println("dirham: " + dirham);
-//        System.out.println("real: " + real);
-//        System.out.println("chilean_peso: " + chilean_peso);
-//        System.out.println("mexican_peso: " + mexican_peso);
-//        System.out.println("_yen: " + _yen);
-//        System.out.println("$australian: " + $australian);
-//        System.out.println("dollar: " + dollar);
-//        System.out.println("Rupee: " + Rupee);
     }
+
+    public static void main(String[] args) {
+        CurrencyConverter cc = new CurrencyConverter();
+        // Jan Ist
+        double[] rates = {63.0, 3.0, 3.0, 595.5, 18.0, 107.0, 2.0};
+        cc.setExchangeRates(rates);
+        cc.printCurrencies();
+
+        //
+        rates = new double[]{65.0, 3.0, 3.0, 595.5, 18.0, 107.0, 2.0};
+        cc.setExchangeRates(rates);
+        cc.printCurrencies();
+
+        cc.updateExchangeRates(0, 66);
+        cc.printCurrencies();
+
+        double amount = cc.computeTransferAmount(0, 1000);
+        System.out.println("\nTransferred Amount: " + amount);
+    }
+
 }
---
+++ src/com/semanticsquare/basics/MoneyTransferService.java
@@ -0,0 +1,2 @@
+package com.semanticsquare.basics;public class MoneyTransferService {
+}
