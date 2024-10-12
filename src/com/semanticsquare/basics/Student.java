3b909e1bedb027d25c7808f006a600a20ea3211e
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-10-12 01:24:24 +0100

    More on Constructors

--- src/com/semanticsquare/basics/Student.java
+++ src/com/semanticsquare/basics/Student.java
@@ -39,36 +39,19 @@ class Student {
             //return;
         }
 
-        System.out.println("\nid: " + this.id);
-        System.out.println("nextId: " + nextId);
-        System.out.println("name: " + this.name);
-        System.out.println("gender: " + this.gender);
-        System.out.println("age: " + this.age);
-        System.out.println("phone: " + this.phone);
-        System.out.println("gpa: " + this.gpa);
-        System.out.println("degree: " + this.degree);
-        System.out.println("tuitionFees: " + tuitionFees);
-        System.out.println("studentCount: " + studentCount);
+//        System.out.println("\nid: " + this.id);
+//        System.out.println("nextId: " + nextId);
+//        System.out.println("name: " + this.name);
+//        System.out.println("gender: " + this.gender);
+//        System.out.println("age: " + this.age);
+//        System.out.println("phone: " + this.phone);
+//        System.out.println("gpa: " + this.gpa);
+//        System.out.println("degree: " + this.degree);
+//        System.out.println("tuitionFees: " + tuitionFees);
+//        System.out.println("studentCount: " + studentCount);
     }
 
     Student() {
-    }
-
-    public static void main(String[] args) {
-        Student student1 = new Student(1000, "Joan", "male", 18, 223_456_7890L, 3.8, 'B');
-
-        Student student2 = new Student(1001, "Raj", "male", 21, 223_456_9999L, 3.4, 'M', true);
-
-        Student student3 = new Student(1002, "Anita", "female", 20, 223_456_8888L, 4.0, 'M', true);
-
-        System.out.println("\nStudent.studentCount: " + Student.studentCount);
-
-        System.out.println("\nName of student 1: " + student1.name);
-        System.out.println("Name of student 2: " + student2.name);
-        System.out.println("Name of student 3: " + student3.name);
-
-        student1.updateProfile("John");
-        System.out.println("\nUpdated name of student 1: " + student1.name);
     }
 
     boolean updateProfile(String name) {
