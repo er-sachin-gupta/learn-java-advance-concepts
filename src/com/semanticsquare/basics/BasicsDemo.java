1718d51a1517a82c64d298c1d1e691c32d27610e
Sachin Gupta <sachin.gupta@finteksoftwareservices.com>
2024-11-30 11:35:26 +0000

    Strings Interning

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -242,6 +242,24 @@ public class BasicsDemo {
         System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
     }
 
+    static void stringPool() {
+        System.out.println("\nInside stringPool ...");
+        String s1 = "hello!";
+        String s2 = "hello!";
+        String s3 = "hello!".intern();
+        String s4 = new String("hello!");
+        String s5 = "lo!";
+        final String s6 = "lo!";
+
+        System.out.println("s1 == s2: " + (s1 == s2));
+        System.out.println("s1 == s3: " + (s1 == s3));
+        System.out.println("s1 == s4: " + (s1 == s4));
+        System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
+        System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
+        System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+        System.out.println("s1 == \"hel\" + s6: " + (s1 == "hel" + s6));
+    }
+
     public static void main(String[] args) {
         // Language Basics 1
         //print();
@@ -259,6 +277,7 @@ public class BasicsDemo {
         //labeledContinue();
         // import demo
         // package creation demo
-        stringExamples();
+//        stringExamples();
+        stringPool();
     }
 }
